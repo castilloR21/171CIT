6936291b2e8f88a59bbdae1775dcef4a3b377136
Ryan Castillo <cas21033@byui.edu>
2022-05-21 17:17:39 -0600

    week5

--- manageuser.js
+++ manageuser.js
@@ -1,7 +1,7 @@
 //© 2021 Sean Murdock
 let phoneNumber = "";
 let userName = "";
-let password = "";
+let oneTimePassword = "";
 let verifypassword = "";
 let passwordRegEx=/((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%!]).{6,40})/;
 
@@ -11,10 +11,8 @@ function setphonenumber(){
 
 function setuserpassword(){
     password = $("#password").val();
-    var valid=passwordRegEx.exec(password);
-    if (!valid){
-        alert('Must be 6 digits, upper, lower, number, and symbol');
-    }
+    
+    
 }
 
 function setverifypassword(){
@@ -63,8 +61,8 @@ function userlogin(){
     setusername();
     $.ajax({
         type: 'POST',
-        url: 'https://dev.stedi.me/login',
-        data: JSON.stringify({userName, password}),
+        url: 'https://dev.stedi.me/twofactorlogin',
+        data: JSON.stringify({phoneNumber, oneTimePassword}),
         success: function(data) {
             window.location.href = "/timer.html#"+data;//add the token to the url
         },
